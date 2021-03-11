789ef417fb6bb5c5964216ff987580a8a6f35dd6
Андрей Жданов <cuter.exe@gmail.com>
2021-03-11 18:47:39 +0300

    refactor: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const bracketsMap = new Map(bracketsConfig);
+    let openedBrackets = [];
+    let closedBrackets = [];
+    function popFromArrays() {
+        openedBrackets.pop();
+        closedBrackets.pop();
+    }
+    function pushToArrays(openedBracket, closedBracket) {
+        openedBrackets.push(openedBracket);
+        closedBrackets.push(closedBracket);
+    }
+
+    for (let symbol of str) {
+        closedBracket = bracketsMap.get(symbol);
+        if (closedBrackets.length > 0 && closedBracket === symbol) {
+            if (symbol === closedBrackets[closedBrackets.length - 1]) {
+                popFromArrays();
+                continue;
+            }
+        }
+        const openBracket = bracketsMap.has(symbol);
+        if (openBracket) {
+            pushToArrays(symbol, closedBracket);
+        } else {
+            if (symbol === closedBrackets[closedBrackets.length - 1]) {
+                popFromArrays();
+            } else {
+                return false;
+            }
+        }
+    }
+    return !openedBrackets.length && !closedBrackets.length;
+};
